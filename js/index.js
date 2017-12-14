b52d65d38320e77ed557bf8e40746598bc4a9600
nchnsn <nchnsn@gmail.com>
2017-12-14 01:51:47 -0800

    css tweaks and discovery card update.

--- js/index.js
+++ js/index.js
@@ -6,11 +6,62 @@ var hitNumber = 5;
 
 var helper = algoliasearchHelper(algolia, 'restaurant_data', {
   facets: ['food_type', 'stars_count', 'payment_options'],
+  disjunctiveFacets:['stars_count', 'payment_options'],
   hitsPerPage: hitNumber,
-  maxValuesPerFacet: 4
+  maxValuesPerFacet: 5
 });
 
+// facet methods
+// star ratings
+var fourStarsToggle = false;
+function fourStarsFacet(){
+  if(fourStarsToggle){
+    helper.clearRefinements().search();
+  } else {
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.1);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.2);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.3);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.4);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.5);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.6);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.7);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.8);
+    helper.addDisjunctiveFacetRefinement('stars_count', 4.9);
+    helper.search()
+  }
+  fourStarsToggle = !fourStarsToggle;
+}
+
+var discoverToggle = false;
+function discoverFacet(){
+  alert('discovvvrrr');
+  if(discoverToggle){
+    helper.clearRefinements().search();
+  } else {
+    helper.addDisjunctiveFacetRefinement('payment_options', 'Discover');
+    helper.addDisjunctiveFacetRefinement('payment_options', 'Diners Club');
+    helper.addDisjunctiveFacetRefinement('payment_options', 'Carte Blanche');
+    helper.search()
+  }
+  discoverToggle = !discoverToggle;
+}
+
+// helper.addDisjunctiveFacetRefinement('stars_count', 4.3);
+
 helper.on("result", searchCallback);
+// helper.on("result", function(results, state){
+//   console.log(results);
+  
+// });
+
+helper.on('searchQueueEmpty', function() {
+  var searchIcon = document.getElementById("search-icon-loading");
+  searchIcon.innerHTML = '<i class="fas fa-search fa-3x"></i>'; 
+  var hideMore = document.getElementById("more-results-div");
+  hideMore.classList.remove("hide-more");
+  console.log('No more search pending');
+  // This is received before the result event if we're not expecting new results
+});
 
 var $inputfield = $("#search-box");
 var $hits = $('#hits');
@@ -75,12 +126,18 @@ function renderFacets($facets, results) {
       facetName = '';
     }
 
-    var header = '<h4>' + facetName + '</h4>';
+    var header = '<h4 style="margin-top:15px">' + facetName + '</h4>';
     var facetValues = results.getFacetValues(name);
     var facetsValuesList = $.map(facetValues, function(facetValue) {
       var facetValueClass = facetValue.isRefined ? 'refined'  : '';
+      console.log(facetValue.name);
       if(facet.name === 'stars_count'){
         var valueAndCount = '<a data-attribute="' + name + '" data-value="' + facetValue.name + '" href="#">' +  facetValue.name + starMaker(facetValue.name) + ' (' + facetValue.count + ')' + '</a>';
+        // var valueAndCount = '<a href="#">' + starMaker(facetValue.name) + '</a>';
+      } else if(facetValue.name === 'Discover'){
+        var valueAndCount = '<a  onclick="discoverFacet()"' + 'href="#">' + facetValue.name + ' (' + facetValue.count + ')' + '</a>';
+      } else if(facetValue.name === 'Diners Club'){
+        var valueAndCount = '';
       } else{
         var valueAndCount = '<a data-attribute="' + name + '" data-value="' + facetValue.name + '" href="#">' + facetValue.name + ' (' + facetValue.count + ')' + '</a>';
       }
@@ -357,3 +414,4 @@ function showMoreHits(){
   var hideMore = document.getElementById("more-results-div");
   hideMore.classList.add("menu-toggle");
 };
+
